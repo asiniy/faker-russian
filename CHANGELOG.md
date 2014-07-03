76803bc078b986aaaedf6e359e4aaf27576d6ed4
Alex Antonov <kaburbundokel11g@inbox.ru>
2014-07-03 17:29:12 +0400

    0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# 0.0.2
+
+* Kpp faker added
+* Inn faker kinds fixed
+
+# 0.0.1
+
+* Inn faker added
--- lib/faker/russian/version.rb
+++ lib/faker/russian/version.rb
@@ -1,7 +1,7 @@
 module Faker
   module Russian
     module Version
-      VERSION = '0.0.1'
+      VERSION = '0.0.2'
 
       def version
         VERSION
