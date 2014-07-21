a0bbe1d651d5d0777ed68693806b33040c2c050f
Alex Antonov <kaburbundokel11g@inbox.ru>
2014-07-21 11:52:40 +0400

    0.0.4 [ci skip]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.0.4
+
+* Bik faker added
+* Okato faker added
+
 # 0.0.3
 
 * Okpo faker added
--- lib/faker/russian/version.rb
+++ lib/faker/russian/version.rb
@@ -1,7 +1,7 @@
 module Faker
   module Russian
     module Version
-      VERSION = '0.0.3'
+      VERSION = '0.0.4'
 
       def version
         VERSION
